1e53fa293f218a015b349740e4517f7127a3e4c7
Tassia Shibuya <tassia.shibuya@gmail.com>
2020-03-21 22:41:56 -0700

    updated the number of the returned result error

--- script.js
+++ script.js
@@ -9,81 +9,77 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-generatePassword()
-
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
+  generatePassword()
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-// When use click the button
 // Prompt question to the user about the length of the password
 var userChoice = prompt("Choose a password length");
 // If in the criteria > 8 and <128
     if (userChoice < 8 || userChoice > 128) {
         alert("Choose a number between 8 and 128");
   } else {
-        alert("Now, confirm if you want in your password:");
-  }
+        alert("Next step...confirm what would you want to include in your password:");
 
-// Confirm if user wants Lowercase
-var confirmLower = confirm("Lowercase?");
-// Confirm if user wants upperCase
-var confirmUpper = confirm("Uppercase?");
-// Confirm if user wants special chars
-var confirmSpecial = confirm("Special Character?");
-// Confirm if user wants numeric chars
-var confirmNum = confirm("Numeric Character?");
+        // Confirm if user wants Lowercase
+        var confirmLower = confirm("Lowercase?");
+        // Confirm if user wants upperCase
+        var confirmUpper = confirm("Uppercase?");
+        // Confirm if user wants special chars
+        var confirmSpecial = confirm("Special Character?");
+        // Confirm if user wants numeric chars
+        var confirmNum = confirm("Numeric Character?");
+    
 
-function generatePassword() {
 
-    var password = []
-    for(var i = password.length; i < userChoice;) {
-    // Check each of the statements above is true
-    console.log(i)
-    console.log(password)
-        if(confirmNum === true && i < userChoice); {
-            var numeric = nums[Math.floor(Math.random() * nums.length)];
-            i++;
-            password.push(numeric);
-            console.log("confirmNum");    
-        }
-        if(confirmLower === true && i < userChoice);{
-            var lower = lowercas[Math.floor(Math.random() * lowercas.length)];
-            i++;
-            password.push(lower);
-            console.log("lower"); 
-        }
-        if(confirmUpper === true && i < userChoice);{
-            var upper = uppercas[Math.floor(Math.random() * uppercas.length)];
-            i++;
-            password.push(upper);
-            console.log("upper")
-        }
-        if(confirmSpecial === true && i < userChoice);{
-            i++;
-            // getRandomSymbol();
-            password.push(getRandomSymbol())
-            console.log("symbol");
-        }
+// Function to generate the password based on the confirmed additions by the user
+    function generatePassword() {
+        var password = []   
+        for(var i = password.length; i < userChoice; i++) {
+    
+        // Check each of the statements above is true
+        // if so bring a random character of for each confirmed
+
+        console.log(password)
+            if(confirmNum === true); {
+                var numeric = nums[Math.floor(Math.random() * nums.length)];
+                password.push(numeric);
+                console.log(confirmNum);    
+            }
+            if(confirmLower === true);{
+                var lower = lowercas[Math.floor(Math.random() * lowercas.length)];
+                password.push(lower);
+                console.log("lower"); 
+            }
+            if(confirmUpper === true);{
+                var upper = uppercas[Math.floor(Math.random() * uppercas.length)];
+                password.push(upper);
+                console.log("upper")
+            }
+            if(confirmSpecial === true);{
+                // getRandomSymbol();
+                password.push(getRandomSymbol())
+                console.log("symbol");
+            }
+        }   
+
+        console.log(password)
+        // Show the result without commas in between the elements
+        NewPass = password.join("");
+        // Only present the lentgh inputed by the user 
+        var finalPassword = NewPass.slice(0, userChoice)
+        return finalPassword
     }
-    console.log(password)
-    NewPass = password.join("");
-    return NewPass
-}
+    }
 
-// // choose one random numeber from the array
-// var numeric = nums[Math.floor(Math.random() * nums.length)];
-// console.log(numeric);
-
-
-// Generate random characters and add it to userPass
+// Generate random symbol characters
 function getRandomSymbol() {
     const symbols = '!@#$%^&*(){}[]=<>/,.'
     return symbols[Math.floor(Math.random() * symbols.length)];
-  };
+  }
